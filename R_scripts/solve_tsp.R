1b88e4bffa9b339c5378a96be7259ddc0ba3a063
Ryan Witko <ryan.witko@blackrock.com>
2018-09-13 17:32:37 -0400

    New script to solve for shortest tour.

---
+++ R_scripts/solve_tsp.R
@@ -0,0 +1,35 @@
+library("TSP")
+
+# Path to file of distances between stations
+dist.file <- "data/distances.csv"
+
+# Read file from disk and turn into a matrix
+df <- read.csv(dist.file)
+mat <- as.matrix(df)
+rownames(mat) <- colnames(mat)
+
+# Ensure that the matrix is symmetric
+mat[lower.tri(mat)] = t(mat)[lower.tri(mat)]
+
+# Create a TSP object to be solved
+tsp <- TSP(mat)
+
+# Add a dummy station with zero distance to all other stations
+tsp <- insert_dummy(tsp, label = "cut")
+
+# Solve for the optimal tour
+ctour <- solve_TSP(tsp, method = "concorde")
+
+# Print the length of the tour
+tour_length(ctour)
+
+# Reassemble the tour around the cut point
+path <- cut_tour(ctour, "cut")
+
+# Print the ordered labels
+labels( path )
+
+
+
+
+
